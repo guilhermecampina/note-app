89ff37feee574cbf54534b705c1b33a96efe7680
Guilherme Campina <campinaguilherme@gmail.com>
2023-07-16 19:34:33 -0300

    v.1

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const addBox = document.querySelector('.add-box');
 const popupBox = document.querySelector('.popup-box');
+const popupTitle = popupBox.querySelector('header p');
 const closeIcon = popupBox.querySelector('header i');
 const addBtn = popupBox.querySelector('button');
 
@@ -10,22 +11,27 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
                 'August', 'September', 'Octuber', 'November', 'December'];
 
 const notes = JSON.parse(localStorage.getItem('notes') || '[]');
+let isUpdate = false, updateId;
 
 
 
 addBox.addEventListener('click', () => {
+    titleTag.focus();
     popupBox.classList.add('show');
 })
 
 closeIcon.addEventListener('click', () => {
+    isUpdate = false;
     titleTag.value = '';
     descTag.value = '';
+    addBtn.innerText = 'Add Note';
+    popupTitle.innerText = 'Add a new Note';
     popupBox.classList.remove('show');
 })
 
 function showNotes() {
     document.querySelectorAll('.note').forEach(note => note.remove());
-    notes.forEach((note) => {
+    notes.forEach((note, index) => {
         let liTag = `<li class="note">
                         <div class="details">
                             <p>${note.title}</p>
@@ -36,8 +42,8 @@ function showNotes() {
                             <div class="settings">
                                 <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>  
                                 <ul class="menu">
-                                    <li><i class="uil uil-pen"></i>Edit</li>
-                                    <li><i class="uil uil-trash"></i>Delete</li>
+                                    <li onclick="updateNote(${index}, '${note.title}', '${note.description}')"><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick="deleteNote(${index})"><i class="uil uil-trash"></i>Delete</li>
                                 </ul>  
                             </div>
                         </div>
@@ -49,7 +55,31 @@ showNotes();
 
 function showMenu(elem) {
     elem.parentElement.classList.add('show');
+    document.addEventListener('click', (e) => {
+        if(e.target.tagName != 'I' || e.target != elem) {
+            elem.parentElement.classList.remove('show');
+        }
+    })
 }
+
+function deleteNote(noteId) {
+    let confirmDel = confirm('Are you sure you want to delete this note?');
+    if(!confirmDel) return;
+    notes.splice(noteId, 1);
+    localStorage.setItem('notes', JSON.stringify(notes));
+    showNotes();
+}
+
+function updateNote(noteId, title, desc) {
+    addBox.click();
+    isUpdate = true;
+    updateId = noteId;
+    titleTag.value = title;
+    descTag.value = desc;
+    addBtn.innerText = 'Update Note';
+    popupTitle.innerText = 'Update a Note';
+    console.log(noteId, title, desc);
+}   
 
 addBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -69,8 +99,13 @@ addBtn.addEventListener('click', (e) => {
 
         }
         
+        if (!isUpdate) {
+            notes.push(noteInfo);
+        } else {
+            isUpdate = false;
+            notes[updateId] = noteInfo;
+        }
         
-        notes.push(noteInfo);
         localStorage.setItem('notes', JSON.stringify(notes));
         closeIcon.click();
         showNotes();
